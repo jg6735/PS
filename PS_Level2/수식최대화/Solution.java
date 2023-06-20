138a736d70c3f5ec3819fe4e6ef139a48fbbd8b0
jg6735 <jg6735@naver.com>
2023-06-20 22:50:13 +0900

    PS Level2 수식 최대화

---
+++ PS_Level2/수식최대화/Solution.java
@@ -0,0 +1,67 @@
+package PS_Level2.수식최대화;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/67257
+class Solution {
+    private static final String[][] precedences = {
+            "+-*".split(""),
+            "+*-".split(""),
+            "-+*".split(""),
+            "-*+".split(""),
+            "*+-".split(""),
+            "*-+".split("")
+    };
+
+    private long calculate(long lhs, long rhs, String op) {
+        switch (op) {
+            case "+":
+                return lhs + rhs;
+            case "-":
+                return lhs - rhs;
+            case "*":
+                return lhs * rhs;
+            default:
+                return 0;
+        }
+    }
+
+    private long calculate(List<String> tokens, String[] precedence) {
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).equals(op)) {
+                    long lhs = Long.parseLong(tokens.get(i - 1));
+                    long rhs = Long.parseLong(tokens.get(i + 1));
+                    long result = calculate(lhs, rhs, op);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+
+        return Long.parseLong(tokens.get(0));
+    }
+
+    public long solution(String expression) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*", true);
+        List<String> tokens = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
+
+        long max = 0;
+        for (String[] precedence : precedences) {
+            long value = Math.abs(calculate(new ArrayList<>(tokens), precedence));
+            if (value > max) {
+                max = value;
+            }
+        }
+
+        return max;
+    }
+}
