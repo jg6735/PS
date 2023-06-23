fdbd97d0d930fbf4a962b86459960f0a8c2e1278
jg6735 <jg6735@naver.com>
2023-06-23 19:40:51 +0900

    PS Level2 구명보트

---
+++ PS_Level2/구명보트/Solution.java
@@ -0,0 +1,25 @@
+package PS_Level2.구명보트;
+
+import java.util.Arrays;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42885
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
