7247e6abfb28a08b39f00b8e57d9ae5d6f861346
jg6735 <jg6735@naver.com>
2023-08-02 23:42:53 +0900

    PS Level2 튜플 Pattern, Matcher 사용

--- PS_Level2/튜플/Solution.java
+++ PS_Level2/튜플/Solution.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/64065
 class Solution {
@@ -21,6 +23,23 @@ class Solution {
         int index = 0;
         for (String str : list) {
             answer[index++] = Integer.parseInt(str);
+        }
+
+        return answer;
+    }
+
+    public int[] solution2(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            String group = matcher.group();
+            map.put(group, map.getOrDefault(group, 0) + 1);
+        }
+
+        int[] answer = new int[map.size()];
+        for (Map.Entry<String, Integer> key : map.entrySet()) {
+            answer[map.size() - key.getValue()] = Integer.parseInt(key.getKey());
         }
 
         return answer;
