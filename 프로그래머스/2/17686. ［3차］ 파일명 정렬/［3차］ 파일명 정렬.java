01eb55428d1c416a09aa1978a3e03a41a58d68e8
Geun Ji <geunji9505@gmail.com>
2024-03-28 11:58:27 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 19.42 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,61 +1,26 @@
 import java.util.*;
+import java.util.regex.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
-        
-        String[][] list = new String[files.length][3];
-        for (int i = 0; i < files.length; i++) {
-            String name = files[i];
-            int head = 0;
-            int number = 0;
-            for (int j = 0; j < name.length(); j++) {
-                if (!Character.isDigit(name.charAt(j)) && Character.isDigit(name.charAt(j + 1))) {
-                    head = j;
-                    number = j + 1;
-                    break;
-                }
-            }
-            
-            for (int j = head + 1; j < head + 5; j++) {
-                if (j + 1 >= name.length()) {
-                    break;
-                }
-                
-                if (Character.isDigit(name.charAt(j))) {
-                    if (Character.isDigit(name.charAt(j + 1))) {
-                        number++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-            
-            list[i][0] = name.substring(0, head + 1);
-            list[i][1] = name.substring(head + 1, number + 1);
-            list[i][2] = name.substring(number + 1);
-        }
-        
-        Arrays.sort(list, (o1, o2) -> {
-            if (o1[0].toUpperCase().equals(o2[0].toUpperCase())) {
-                int a = Integer.parseInt(o1[1]);
-                int b = Integer.parseInt(o2[1]);
-                
-                if (a == b) {
-                    return 0;
-                }
-                
-                return a - b;
-            }
-            
-            return o1[0].toUpperCase().compareTo(o2[0].toUpperCase());
-        });
-        
-        for (int i = 0; i < list.length; i++) {
-            answer[i] = list[i][0] + list[i][1] + list[i][2];
-        }
-        
-        return answer;
-    }
+        Pattern p = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
 
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                Matcher m1 = p.matcher(s1.toLowerCase());
+                Matcher m2 = p.matcher(s2.toLowerCase());
+                m1.find();
+                m2.find();
+
+                if(!m1.group(1).equals(m2.group(1))) {
+                    return m1.group(1).compareTo(m2.group(1));
+                } else {
+                    return Integer.parseInt(m1.group(2)) - Integer.parseInt(m2.group(2));
+                }
+            }
+        });
+
+        return files;
+    }
 }
