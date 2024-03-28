5a0b93336b48c8fccfb7a846eb5357a2a98cdafa
Geun Ji <geunji9505@gmail.com>
2024-03-28 10:31:46 +0900

    [level 2] Title: [3차] 압축, Time: 1.20 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -2,38 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String msg) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> dictionary = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        
         int idx = 1;
         for (char c = 'A'; c <= 'Z'; c++) {
-            map.put(Character.toString(c), idx++);
+            dictionary.put(Character.toString(c), idx++);
         }
         
-        char[] arr = msg.toCharArray();
-        List<Integer> result = new ArrayList<>();
-        int n = 0;
-        for (int i = 0; i < arr.length; i++) {
-            char c = arr[i];
-            StringBuilder sb = new StringBuilder(Character.toString(c));
-            n = map.get(sb.toString());
+        int prev = 0;
+        StringBuilder builder;
+        for (int i = 0; i < msg.length(); i++) {
+            char cur = msg.charAt(i);
+            builder = new StringBuilder(Character.toString(cur));
+            prev = dictionary.get(builder.toString());
             int count = 0;
-            for (int j = i + 1; j < arr.length; j++) {
-                char next = arr[j];
-                sb.append(next);
-                if (!map.containsKey(sb.toString())) {
-                    result.add(n);
-                    map.put(sb.toString(), idx++);
+            for (int j = i + 1; j < msg.length(); j++) {
+                char next = msg.charAt(j);
+                builder.append(next);
+                
+                if (!dictionary.containsKey(builder.toString())) {
+                    dictionary.put(builder.toString(), idx++);
+                    result.add(prev);
                     break;
-                } else {
-                    n = map.get(sb.toString());
-                    count++;
                 }
+                
+                prev = dictionary.get(builder.toString());
+                count++;
             }
             
             i += count;
         }
         
-        if (n != 0) {
-            result.add(n);
+        if (prev != 0) {
+            result.add(prev);
         }
 
         int[] answer = new int[result.size()];
