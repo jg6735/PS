4b7b9e5adcea9f7b66895487ac8674b660ebe06b
Geun Ji <geunji9505@gmail.com>
2024-03-29 14:02:35 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 5.63 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,67 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        
-        Map<String, Map<String, Integer>> nameMap = new HashMap<>();
-        Map<String, Integer> fromMap = new HashMap<>();
-        Map<String, Integer> toMap = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < friends.length; i++) {
-            String name = friends[i];
-            fromMap.put(name, 0);
-            toMap.put(name, 0);
-            Map<String, Integer> map = new HashMap<>();
-            for (int j = 0; j < friends.length; j++) {
-                if (i != j) {
-                    map.put(friends[j], 0);
-                    nameMap.put(name, map);
-                }
-            }
+            map.put(friends[i], i);
         }
-        
-        StringTokenizer st;
-        for (String gift : gifts) {
-            st = new StringTokenizer(gift);
-            String from = st.nextToken();
-            String to = st.nextToken();
-            fromMap.put(to, fromMap.get(to) + 1);
-            toMap.put(from, toMap.get(from) + 1);
-            nameMap.get(from).put(to, nameMap.get(from).get(to) + 1);
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+
+        for (String str : gifts) {
+            String[] cur = str.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;
         }
-        
-        int[] arr = new int[friends.length];
-        for (int i = 0; i < friends.length; i++) {
-            String myName = friends[i];
-            for (int j = i + 1; j < friends.length; j++) {
-                String friend = friends[j];
-                int a = nameMap.get(myName).get(friend);
-                int b = nameMap.get(friend).get(myName);
-                
-                if (a > b) {
-                    nameMap.get(myName).put(friend, nameMap.get(myName).get(friend) + 1);
-                    arr[i]++;
-                } else if (b > a) {
-                    nameMap.get(friend).put(myName, nameMap.get(friend).get(myName) + 1);
-                    arr[j]++;
-                } else if ((a == 0 && b == 0) || a == b) {
-                    int aCount = toMap.get(myName) - fromMap.get(myName);
-                    int bCount = toMap.get(friend) - fromMap.get(friend);
-                    
-                    if (aCount > bCount) {
-                        nameMap.get(friend).put(myName, nameMap.get(friend).get(myName) + 1);
-                        arr[i]++;
-                    } else if (bCount > aCount) {
-                        nameMap.get(myName).put(friend, nameMap.get(myName).get(friend) + 1);
-                        arr[j]++;
-                    }
-                }
-            }
-        }
-        
-        for (int num : arr) {
-            answer = Math.max(answer, num);
-        }
-        
-        return answer;
+
+       int ans = 0;
+       for (int i = 0; i < friends.length; i++) {
+           int cnt = 0;
+           for (int j = 0; j < friends.length; j++) {
+               if(i == j) continue;
+               if (record[i][j] > record[j][i]) cnt++;
+               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
+           }
+           ans = Math.max(cnt, ans);
+       }
+        return ans;
     }
 }
