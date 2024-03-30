8e3b7a6e8eee782931dad2f24400fb48b4e56967
Geun Ji <geunji9505@gmail.com>
2024-03-30 10:42:59 +0900

    [level 2] Title: 단체사진 찍기, Time: 3490.04 ms, Memory: 384 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,79 +1,42 @@
 import java.util.*;
-
 class Solution {
-    private static final char[] CHARACTERS = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-    
-    private static int answer;
-    private static boolean[] isSelected;
-    private static char[][] conditions;
-    private static Map<Character, Integer> map;
-    
+    static String[] arr = {"A","C","F","J","M","N","R","T"};
+    static String[] result = new String[8];
+    static boolean[] used = new boolean[8];
+    static int answer;
     public int solution(int n, String[] data) {
-        init(n, data);
-        search(0, conditions);
+        answer = 0;
+        perm(0,data);
         return answer;
     }
-    
-    private void init(int n, String[] data) {
-        answer = 0;
-        isSelected = new boolean[8];
-        map = new HashMap<>();
-        conditions = initCond(n, data);
-    }
-    
-    private char[][] initCond(int n, String[] data) {
-        conditions = new char[n][4];
-        for (int i = 0; i < n; i++) {
-            String d = data[i];
-            conditions[i][0] = data[i].charAt(0);
-            conditions[i][1] = data[i].charAt(2);
-            conditions[i][2] = data[i].charAt(3);
-            conditions[i][3] = data[i].charAt(4);
-        }
-        
-        return conditions;
-    }
-    
-    private void search(int cnt, char[][] conditions) {
-        if (cnt == 8) {
-            for (char[] condition : conditions) {
-                char a = condition[0];
-                char b = condition[1];
-                char op = condition[2];
-                int size = condition[3] - '0';
-                
-                int aIdx = map.get(a);
-                int bIdx = map.get(b);
-                if (!isAvailable(aIdx, bIdx, op, size)) {
+    static void perm(int cnt, String[] data){
+        if(cnt==8){
+            String s = "";
+            for(int i=0; i<arr.length; i++){
+                s+=result[i];
+            }
+            for(int i=0; i<data.length; i++){
+                int start = s.indexOf(data[i].charAt(0));
+                int end = s.indexOf(data[i].charAt(2));
+
+                if(data[i].charAt(3)=='=' && Math.abs(start-end)-1!=data[i].charAt(4)-'0'){
+                    return;
+                }else if(data[i].charAt(3)=='<' && Math.abs(start-end)-1>=data[i].charAt(4)-'0'){
+                    return;
+                }else if(data[i].charAt(3)=='>' && Math.abs(start-end)-1<=data[i].charAt(4)-'0'){
                     return;
                 }
             }
-            
             answer++;
             return;
         }
-        
-        for (int i = 0; i < 8; i++) {
-            if (isSelected[i]) {
-                continue;
+        for(int i=0; i<arr.length; i++){
+            if(!used[i]){
+                used[i] = true;
+                result[cnt] = arr[i];
+                perm(cnt+1, data);
+                used[i] = false;
             }
-            
-            map.put(CHARACTERS[i], cnt);
-            isSelected[i] = true;
-            search(cnt + 1, conditions);
-            isSelected[i] = false;
-        }
-    }
-    
-    private boolean isAvailable(int aIdx, int bIdx, char op, int size) {
-        if (op == '=' && Math.abs(aIdx - bIdx) == size + 1) {
-            return true;
-        } else if (op == '<' && Math.abs(aIdx - bIdx) < size + 1) {
-            return true;
-        } else if (op == '>' && Math.abs(aIdx - bIdx) > size + 1) {
-            return true;
-        } else {
-            return false;
         }
     }
 }
