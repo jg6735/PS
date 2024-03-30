a6981757c537b14bb26a81576733f245bcf00d73
Geun Ji <geunji9505@gmail.com>
2024-03-30 10:43:28 +0900

    [level 2] Title: 단체사진 찍기, Time: 1596.18 ms, Memory: 217 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,42 +1,88 @@
-import java.util.*;
 class Solution {
-    static String[] arr = {"A","C","F","J","M","N","R","T"};
-    static String[] result = new String[8];
-    static boolean[] used = new boolean[8];
-    static int answer;
-    public int solution(int n, String[] data) {
-        answer = 0;
-        perm(0,data);
-        return answer;
-    }
-    static void perm(int cnt, String[] data){
-        if(cnt==8){
-            String s = "";
-            for(int i=0; i<arr.length; i++){
-                s+=result[i];
-            }
-            for(int i=0; i<data.length; i++){
-                int start = s.indexOf(data[i].charAt(0));
-                int end = s.indexOf(data[i].charAt(2));
+  public int solution(int n, String[] data) {
+        char[] s = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
 
-                if(data[i].charAt(3)=='=' && Math.abs(start-end)-1!=data[i].charAt(4)-'0'){
-                    return;
-                }else if(data[i].charAt(3)=='<' && Math.abs(start-end)-1>=data[i].charAt(4)-'0'){
-                    return;
-                }else if(data[i].charAt(3)=='>' && Math.abs(start-end)-1<=data[i].charAt(4)-'0'){
-                    return;
+        int count = 0;
+
+        for (int a = 0; a < s.length; a++) {
+            for (int b = 0; b < s.length; b++) {
+                if (b == a) {
+                    continue;
                 }
+                for (int c = 0; c < s.length; c++) {
+                    if (c == a || c == b) {
+                        continue;
+                    }
+                    for (int d = 0; d < s.length; d++) {
+                        if (d == c || d == a || d == b) {
+                            continue;
+                        }
+                        for (int e = 0; e < s.length; e++) {
+                            if (e == d || e == a || e == b || e == c) {
+                                continue;
+                            }
+                            for (int f = 0; f < s.length; f++) {
+                                if (f == e || f == d || f == c || f == b || f == a) {
+                                    continue;
+                                }
+                                for (int g = 0; g < s.length; g++) {
+                                    if (g == a || g == b || g == c || g == d || g == e || g == f) {
+                                        continue;
+                                    }
+                                    for (int h = 0; h < s.length; h++) {
+                                        if (h == a || h == b || h == c || h == d || h == e || h == f || h == g) {
+                                            continue;
+                                        }
+                                        String makeS = "" + s[a] + s[b] + s[c] + s[d] + s[e] + s[f] + s[g] + s[h];
+
+                                        boolean isCheck = false;
+                                        for (int i = 0; i < data.length; i++) {
+                                            char first = data[i].charAt(0);
+                                            char second = data[i].charAt(2);
+                                            char inequality = data[i].charAt(3);
+                                            int distance = data[i].charAt(4) - 48;
+
+                                            int firstIndex = makeS.indexOf(first);
+                                            int secondIndex = makeS.indexOf(second);
+                                            switch (inequality) {
+                                                case '=': {
+                                                    if (Math.abs(firstIndex - secondIndex) == distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                                case '>': {
+                                                    if (Math.abs(firstIndex - secondIndex) > distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                                case '<': {
+                                                    if (Math.abs(firstIndex - secondIndex) < distance + 1) {
+                                                        isCheck = true;
+                                                    } else
+                                                        isCheck = false;
+                                                    break;
+                                                }
+                                            }
+                                            if (isCheck == false) {
+                                                break;
+                                            }
+                                        }
+                                        if (isCheck) {
+                                            count++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
             }
-            answer++;
-            return;
         }
-        for(int i=0; i<arr.length; i++){
-            if(!used[i]){
-                used[i] = true;
-                result[cnt] = arr[i];
-                perm(cnt+1, data);
-                used[i] = false;
-            }
-        }
-    }
+        return count;
+  }
 }
