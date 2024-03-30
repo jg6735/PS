9615795e445ac7f4e2842597219bd481dc865fda
Geun Ji <geunji9505@gmail.com>
2024-03-30 10:43:47 +0900

    [level 2] Title: 단체사진 찍기, Time: 943.75 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,88 +1,79 @@
+import java.util.*;
+
 class Solution {
-  public int solution(int n, String[] data) {
-        char[] s = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-
-        int count = 0;
-
-        for (int a = 0; a < s.length; a++) {
-            for (int b = 0; b < s.length; b++) {
-                if (b == a) {
-                    continue;
-                }
-                for (int c = 0; c < s.length; c++) {
-                    if (c == a || c == b) {
-                        continue;
-                    }
-                    for (int d = 0; d < s.length; d++) {
-                        if (d == c || d == a || d == b) {
-                            continue;
-                        }
-                        for (int e = 0; e < s.length; e++) {
-                            if (e == d || e == a || e == b || e == c) {
-                                continue;
-                            }
-                            for (int f = 0; f < s.length; f++) {
-                                if (f == e || f == d || f == c || f == b || f == a) {
-                                    continue;
-                                }
-                                for (int g = 0; g < s.length; g++) {
-                                    if (g == a || g == b || g == c || g == d || g == e || g == f) {
-                                        continue;
-                                    }
-                                    for (int h = 0; h < s.length; h++) {
-                                        if (h == a || h == b || h == c || h == d || h == e || h == f || h == g) {
-                                            continue;
-                                        }
-                                        String makeS = "" + s[a] + s[b] + s[c] + s[d] + s[e] + s[f] + s[g] + s[h];
-
-                                        boolean isCheck = false;
-                                        for (int i = 0; i < data.length; i++) {
-                                            char first = data[i].charAt(0);
-                                            char second = data[i].charAt(2);
-                                            char inequality = data[i].charAt(3);
-                                            int distance = data[i].charAt(4) - 48;
-
-                                            int firstIndex = makeS.indexOf(first);
-                                            int secondIndex = makeS.indexOf(second);
-                                            switch (inequality) {
-                                                case '=': {
-                                                    if (Math.abs(firstIndex - secondIndex) == distance + 1) {
-                                                        isCheck = true;
-                                                    } else
-                                                        isCheck = false;
-                                                    break;
-                                                }
-                                                case '>': {
-                                                    if (Math.abs(firstIndex - secondIndex) > distance + 1) {
-                                                        isCheck = true;
-                                                    } else
-                                                        isCheck = false;
-                                                    break;
-                                                }
-                                                case '<': {
-                                                    if (Math.abs(firstIndex - secondIndex) < distance + 1) {
-                                                        isCheck = true;
-                                                    } else
-                                                        isCheck = false;
-                                                    break;
-                                                }
-                                            }
-                                            if (isCheck == false) {
-                                                break;
-                                            }
-                                        }
-                                        if (isCheck) {
-                                            count++;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-            }
+    private static final char[] CHARACTERS = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    
+    private static int answer;
+    private static boolean[] isSelected;
+    private static char[][] conditions;
+    private static Map<Character, Integer> map;
+    
+    public int solution(int n, String[] data) {
+        init(n, data);
+        search(0, conditions);
+        return answer;
+    }
+    
+    private void init(int n, String[] data) {
+        answer = 0;
+        isSelected = new boolean[8];
+        map = new HashMap<>();
+        conditions = initCond(n, data);
+    }
+    
+    private char[][] initCond(int n, String[] data) {
+        conditions = new char[n][4];
+        for (int i = 0; i < n; i++) {
+            String d = data[i];
+            conditions[i][0] = data[i].charAt(0);
+            conditions[i][1] = data[i].charAt(2);
+            conditions[i][2] = data[i].charAt(3);
+            conditions[i][3] = data[i].charAt(4);
         }
-        return count;
-  }
+        
+        return conditions;
+    }
+    
+    private void search(int cnt, char[][] conditions) {
+        if (cnt == 8) {
+            for (char[] condition : conditions) {
+                char a = condition[0];
+                char b = condition[1];
+                char op = condition[2];
+                int size = condition[3] - '0';
+                
+                int aIdx = map.get(a);
+                int bIdx = map.get(b);
+                if (!isAvailable(aIdx, bIdx, op, size)) {
+                    return;
+                }
+            }
+            
+            answer++;
+            return;
+        }
+        
+        for (int i = 0; i < 8; i++) {
+            if (isSelected[i]) {
+                continue;
+            }
+            
+            map.put(CHARACTERS[i], cnt);
+            isSelected[i] = true;
+            search(cnt + 1, conditions);
+            isSelected[i] = false;
+        }
+    }
+    
+    private boolean isAvailable(int aIdx, int bIdx, char op, int size) {
+        if (op == '=' && Math.abs(aIdx - bIdx) == size + 1) {
+            return true;
+        } else if (op == '<' && Math.abs(aIdx - bIdx) < size + 1) {
+            return true;
+        } else if (op == '>' && Math.abs(aIdx - bIdx) > size + 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
