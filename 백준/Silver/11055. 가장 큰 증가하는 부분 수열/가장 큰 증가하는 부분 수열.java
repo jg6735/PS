8d67ac6922f7c86035f7da68a24c708aff8b6d53
Geun Ji <geunji9505@gmail.com>
2024-04-25 19:39:30 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 100 ms, Memory: 11992 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N, max;
+    private static int[] arr, dp;
+
+    private static BufferedReader in;
+    private static BufferedWriter out;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+
+    private static void solve() {
+        dp[0] = arr[0];
+        for (int i = 0; i < N; i++) {
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+                }
+            }
+        }
+
+        for (int num : dp) {
+            max = Math.max(num, max);
+        }
+    }
+
+    private static void init() throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(in.readLine());
+        arr = new int[N];
+        dp = new int[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static void print() throws IOException {
+        out.write(Integer.toString(max));
+        out.flush();
+    }
+}
