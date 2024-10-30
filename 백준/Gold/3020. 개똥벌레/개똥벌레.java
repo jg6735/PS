baf704787c2342f1ded28983d9c0812ab13d1419
Geun Ji <geunji9505@gmail.com>
2024-10-30 14:16:54 +0900

    [Gold V] Title: 개똥벌레, Time: 376 ms, Memory: 32144 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -2,9 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -23,15 +21,22 @@ public class Main {
         Arrays.sort(bottom);
 
         int min = Integer.MAX_VALUE;
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (int target = 1; target <= H; target++) {
-            int bottomCount = N / 2 - search(target, bottom, N);
-            int topCount = N / 2 - search(H - target + 1, top, N);
-            min = Math.min(min, bottomCount + topCount);
-            map.put(bottomCount + topCount, map.getOrDefault(bottomCount + topCount, 0) + 1);
+        int minCount = 0;
+        for (int h = 1; h <= H; h++) {
+            int bottomCount = N / 2 - search(h, bottom, N);
+            int topCount = N / 2 - search(H - h + 1, top, N);
+            int count = bottomCount + topCount;
+            if (count < min) {
+                min = count;
+                minCount = 1;
+            } else if (count == min) {
+                minCount++;
+            }
         }
 
-        System.out.print(min + " " + map.get(min));
+        StringBuilder builder = new StringBuilder();
+        builder.append(min).append(" ").append(minCount);
+        System.out.print(builder);
     }
 
     private static int search(int target, int[] arr, int N) {
