59a13a0ef672e11205656bd8c1ad761f1967d9a8
Geun Ji <geunji9505@gmail.com>
2024-11-07 19:11:37 +0900

    [Silver II] Title: 과일 탕후루, Time: 356 ms, Memory: 36296 KB -BaekjoonHub

---
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
+        int left = 0;
+        for (int right = 0; right < N; right++) {
+            map.put(arr[right], map.getOrDefault(arr[right], 0) + 1);
+            while (map.size() > 2) {
+                map.put(arr[left], map.get(arr[left]) - 1);
+                if (map.get(arr[left]) == 0) {
+                    map.remove(arr[left]);
+                }
+                
+                left++;
+            }
+            
+            answer = Math.max(answer, right - left + 1);
+        }
+
+        System.out.print(answer);
+    }
+}
