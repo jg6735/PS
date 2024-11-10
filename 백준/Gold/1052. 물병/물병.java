9bc6f78cdb878a82e04ca365ae0bdac5a4de2340
Geun Ji <geunji9505@gmail.com>
2024-11-10 16:59:13 +0900

    [Gold V] Title: 물병, Time: 80 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        while (Integer.bitCount(N) > K) {
+            N++;
+            answer++;
+        }
+
+        System.out.print(answer);
+    }
+}
