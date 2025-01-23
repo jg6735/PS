ea57ac6d5ea7618c740d0465ac1221ad510298f1
Geun Ji <geunji9505@gmail.com>
2025-01-23 15:30:43 +0900

    [Gold V] Title: 집합의 표현, Time: 352 ms, Memory: 58084 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static final String YES = "YES";
+    static final String NO = "NO";
+
+    static int[] parent, rank;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(in.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (op == 0) {
+                union(a, b);
+            } else {
+                if (find(a) == find(b)) {
+                    builder.append(YES);
+                } else {
+                    builder.append(NO);
+                }
+
+                builder.append("\n");
+            }
+        }
+
+        System.out.print(builder);
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+
+        return parent[x];
+    }
+
+    static void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else if (rank[rootY] > rank[rootX]) {
+                parent[rootX] = rootY;
+            } else {
+                parent[rootX] = rootY;
+                rank[rootX]++;
+            }
+        }
+    }
+}
