c27349cfe32301b3b028f65972b0c775799784f7
Geun Ji <geunji9505@gmail.com>
2025-02-13 19:11:23 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 72 ms, Memory: 11916 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(in.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] lastIdx = new int[N];
+        int[] prev = new int[N];
+        Arrays.fill(prev, -1);
+
+        int len = 0;
+        for (int i = 0; i < N; i++) {
+            int pos = binarySearch(arr, lastIdx, len, i);
+
+            if (pos > 0) {
+                prev[i] = lastIdx[pos - 1];
+            }
+
+            lastIdx[pos] = i;
+            if (pos == len) {
+                len++;
+            }
+        }
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        int idx = lastIdx[len - 1];
+        while (idx != -1) {
+            deque.push(arr[idx]);
+            idx = prev[idx];
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(len).append("\n");
+        while (!deque.isEmpty()) {
+            builder.append(deque.pop()).append(" ");
+        }
+
+        System.out.print(builder);
+    }
+
+    static int binarySearch(int[] arr, int[] lastIdx, int len, int i) {
+        int left = 0;
+        int right = len;
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (arr[lastIdx[mid]] < arr[i]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
